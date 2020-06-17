f89a4eaa4db42adbf3400dd74aea3a3eaa7ecc94
Drew Csontos <35618647+drewcsontos@users.noreply.github.com>
2020-06-17 19:03:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ document.addEventListener("keydown", function (event) {
   if (event.code == "KeyW" || event.code == "Space")
     if (bird.speed < 60) bird.speed = 120;
 });
+
+document.addEventListener("click", function (event) {
+  if (event.code == "KeyW" || event.code == "Space")
+    if (bird.speed < 60) bird.speed = 120;
+});
+
 run(ctx);
 function run(timestamp) {
   let deltatime = timestamp - lastTime;
