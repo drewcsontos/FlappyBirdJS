593c4ddd0d368469862eb06a1f914e268620786a
drewcsontos <drew@csontos.net>
2020-06-25 07:27:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,18 @@ if (
     navigator.userAgent
   )
 ) {
+  var lastTouchEnd = 0;
+  document.addEventListener(
+    "touchend",
+    function (event) {
+      var now = new Date().getTime();
+      if (now - lastTouchEnd <= 300) {
+        event.preventDefault();
+      }
+      lastTouchEnd = now;
+    },
+    false
+  );
   document.addEventListener(
     "touchmove",
     function (event) {
